481695367b09380265d6fa09ffaed3b4dfd2ef26
renzhipeng@mail.dspread.com <Rsdjfkl123465>
2025-06-13 12:00:41 +0800

    adding Demo of HTTP setting time.

--- src/module/demo/use_demo.c
+++ src/module/demo/use_demo.c
@@ -55,3 +55,40 @@ void SnWriteDemo(){
     LOG("[3]get ret = %d sn %d= %s",ret,snLen, temp);
 }
 
+
+
+const char http_date [] = "Wed, 11 Jun 2025 10:15:47 GMT";
+int http_date_to_set(const char *http_date) {
+    if (!http_date ) {
+        return -1;
+    }
+    struct tm tm_time;
+	
+    char buf[128];
+    char weekday[10], month[10], zone[10];
+    int day, year, hour, min, sec;
+    
+    strncpy(buf, http_date, sizeof(buf) - 1);
+    buf[sizeof(buf) - 1] = '\0';
+    
+    if (sscanf(buf, "%s %d %s %d %d:%d:%d %s", 
+               weekday, &day, month, &year, &hour, &min, &sec, zone) != 8) {
+        return -1;
+    }
+    LOG("=weekday=%s d= %d m=%s y=%d h%d f%d s%d==\n",weekday,day,month,year,hour,min,sec);
+    memset(&tm_time, 0, sizeof(struct tm));
+    
+    if (strcmp(month, "Jan") == 0) tm_time.tm_mon = 0;
+    else if (strcmp(month, "Feb") == 0) tm_time.tm_mon = 1;
+    else if (strcmp(month, "Mar") == 0) tm_time.tm_mon = 2;
+    else if (strcmp(month, "Apr") == 0) tm_time.tm_mon = 3;
+    else if (strcmp(month, "May") == 0) tm_time.tm_mon = 4;
+    else if (strcmp(month, "Jun") == 0) tm_time.tm_mon = 5;
+    else if (strcmp(month, "Jul") == 0) tm_time.tm_mon = 6;
+    else if (strcmp(month, "Aug") == 0) tm_time.tm_mon = 7;
+    else if (strcmp(month, "Sep") == 0) tm_time.tm_mon = 8;
+    else if (strcmp(month, "Oct") == 0) tm_time.tm_mon = 9;
+    else if (strcmp(month, "Nov") == 0) tm_time.tm_mon = 10;
+    else if (strcmp(month, "Dec") == 0) tm_time.tm_mon = 11;
+    return 0;
+}
